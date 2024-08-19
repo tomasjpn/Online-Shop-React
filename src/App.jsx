a08fdae053c60fa2efa4ece76c79dbf36df46c2d
AzureAD\PhamTomas <tomasphammc@gmail.com>
2024-08-19 21:21:54 +0200

    ./src: App.jsx: Github Verlinkung; ../styles: App.css:  module.css um mehr Kontrolle über das Styling zu haben; Gallery.module.css: Die Vorschau bei der Galerie getrennt twischen Grafik/Motion Design Katerogie

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
+import image from "./assets/25231.png";
 import Navbar from "./components/Navbar";
 import "./styles/App.css";
+import styles from "./styles/Home.module.css";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="main-area">
-        <div className="main-area-title">
+      <div className={styles.mainArea}>
+        <div className={styles.mainAreaTitle}>
           <h1>Tomas Pham</h1>
           <p>Graphic Design Portfolio</p>
           <Link to="/gallery">
             <button>Explore</button>
           </Link>
+          <br></br>
+          <a
+            href="https://github.com/tomasjpn?tab=repositories"
+            className={styles.linkToGitHub}
+          >
+            <img src={image}></img>
+          </a>
         </div>
       </div>
     </>
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,10 +1,31 @@
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
-
+import styles from "../styles/Gallery.module.css";
 const Gallery = () => {
   return (
     <div>
       <Navbar />
-      <h1> Gallery</h1>
+      <div className={styles.mainArea}>
+        <div className={styles.optionGallery}>
+          <div className={styles.leftSide}>
+            <h1>G R A F I K D E S I G N</h1>
+            <p>
+              Projekte mit Photoshop CS6 (Avatar,Banner, Ads,Präsentationen)
+            </p>
+          </div>
+          <div className={styles.rightSide}>
+            <h1>M O T I O N D E S I G N</h1>
+            <p>
+              Übungen mit After Effects CC (amv,motion design, Schulprojekte)
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.storeButton}>
+        <Link to="/store">
+          <button> Store </button>
+        </Link>
+      </div>
     </div>
   );
 };
