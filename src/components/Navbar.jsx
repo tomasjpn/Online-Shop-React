1a62a594ec06ebf204ed4386b0f0ce818c73bb8a
AzureAD\PhamTomas <tomasphammc@gmail.com>
2024-08-19 22:35:59 +0200

    ../styles: Navbar.css: Die  Navigationstaste ShoppingCart sowie Searchbar wurden Icons ersetzt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,13 @@ const Navbar = () => {
       <br></br>
       <Link to="/gallery">Gallery</Link>
       <br></br>
-      <Link to="/searchpage">Search</Link>
+      <Link to="/searchpage" className="search-page">
+        <img></img>
+      </Link>
       <br></br>
-      <Link to="/shoppingcart">Sopping Cart</Link>
+      <Link to="/shoppingcart" className="shopping-cart">
+        <img></img>
+      </Link>
     </div>
   );
 };
