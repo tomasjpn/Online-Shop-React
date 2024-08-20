beecff1dbc23e11b546430c5bd65b3b34b024e23
AzureAD\PhamTomas <tomasphammc@gmail.com>
2024-08-20 15:57:17 +0200

    ../styles: Gallery.module.jsx, Home.module.jsx: Animation für die Übergange zwischen der Navigation der Seiten. Der Üergang zwischen Homepage und Gallery besteht darin, dass der Inhalt der Homepage nach oben gezogen wird und seitliche von links kommt der Inhalt der Gallerie

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import image from "./assets/25231.png";
 import Navbar from "./components/Navbar";
 import "./styles/App.css";
 import styles from "./styles/Home.module.css";
 
 function App() {
+  // useState um die Animation zu togglen
+  const [isAnimating, setIsAnimating] = useState(false);
+  const navigate = useNavigate(); // Hook um die Seiten zu navigieren
+
+  const handleExploreClick = () => {
+    setIsAnimating(true); // Animation = true -> Slide Up
+    setTimeout(() => {
+      navigate("/gallery"); // Wechselt die Seite nach der Verzögerung
+    }, 1000);
+  };
+
   return (
-    <div>
+    <div className={isAnimating ? styles.slideUpEnd : ""}>
       <Navbar />
       <div className={styles.mainArea}>
         <div className={styles.mainAreaTitle}>
           <h1>Tomas Pham</h1>
           <p>Graphic Design Portfolio</p>
-          <Link to="/gallery">
-            <button>Explore</button>
-          </Link>
+          <button onClick={handleExploreClick}>Explore</button>
           <br></br>
           <a
             href="https://github.com/tomasjpn?tab=repositories"
             className={styles.linkToGitHub}
           >
-            <img src={image}></img>
+            <img src={image} alt="GitHub Link" />
           </a>
         </div>
       </div>
