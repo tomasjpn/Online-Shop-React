ef321806ddc2ca5156f9ddf2aa21e8b51827db8e
AzureAD\PhamTomas <tomasphammc@gmail.com>
2024-08-20 11:53:53 +0200

    ../styles: index.css, ../pages: Gallery.jsx: Animation für den Hintergrund wurde hinzugefügt -> Je nachdem welche Option Bar man auswählt, ändert sich der Hintergrund mit der passenden Darstellung der Kategorie:Grafik Design oder Motion Design

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,30 +1,70 @@
 import { Link } from "react-router-dom";
+import backgroundImage1 from "../assets/GFX.png";
+import backgroundImage2 from "../assets/VFX.png";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Gallery.module.css";
+
 const Gallery = () => {
+  // Funktion um das Bild je nachdem, ob das like oder rechte Feld ausgewählt wird, wird der Hintergrund verändert
+  const handleMouseEnter = (event) => {
+    // event -> um das derzeit ausgewählte Feld zu identifizieren
+    const targetClassName = event.currentTarget.className;
+
+    // Linkes Feld
+    if (targetClassName.includes(styles.leftSide)) {
+      document.documentElement.style.setProperty(
+        "--background-image", // CSS Variable
+        `url(${backgroundImage1})` // Bild URL
+      );
+      // Rechtes Feld
+    } else if (targetClassName.includes(styles.rightSide)) {
+      document.documentElement.style.setProperty(
+        "--background-image", // CSS Variable
+        `url(${backgroundImage2})` // Bild URL
+      );
+    }
+
+    document.documentElement.style.setProperty("--background-blur", "10px"); // Der Background Blur wird gesetzt
+    document.body.classList.add("background-visible"); // Toggle: Sichbarkeit des Backgrounds AN
+  };
+
+  const handleMouseLeave = () => {
+    document.body.classList.remove("background-visible"); // Toggle: Sichtbarkeit des Backgrounds AUS
+  };
+
   return (
     <div>
       <Navbar />
       <div className={styles.mainArea}>
         <div className={styles.optionGallery}>
-          <div className={styles.leftSide}>
+          {/*Linkes Feld der Option Bar für GFX bzw. VFX*/}
+          <div
+            className={styles.leftSide}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
             <h1>GRAFIK DESIGN</h1>
             <p>
-              Projekte mit Photoshop CS6 (Avatar,Banner, Ads,Präsentationen)
+              Projekte mit Photoshop CS6 (Avatar, Banner, Ads, Präsentationen)
             </p>
           </div>
-          <div className={styles.rightSide}>
+          {/*Rectes Feld der Option Bar für GFX bzw. VFX*/}
+          <div
+            className={styles.rightSide}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
             <h1>MOTION DESIGN</h1>
             <p>
-              Übungen mit After Effects CC (amv,motion design, Schulprojekte)
+              Übungen mit After Effects CC (amv, motion design, Schulprojekte)
             </p>
           </div>
         </div>
       </div>
       <div className={styles.storeButton}>
-        <h1>S T O R E</h1>
+        <h2>S T O R E</h2>
         <Link to="/store">
-          <button> Store </button>
+          <button>Store</button>
         </Link>
       </div>
     </div>
